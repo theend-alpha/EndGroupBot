dc0ef7849e5b0986ae05399f0ddd1f57f700ade5
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-07 18:43:31 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -14,7 +14,7 @@ async def start(bot, msg):
 		reply_markup=InlineKeyboardMarkup(Data.buttons)
 	)
 
-@Client.on_message(filters.private & filters.incoming & filters.command("start"))
+@Client.on_message(filters.group & filters.edited & filters.command("start"))
 async def start(bot, msg):
 	user = await bot.get_me()
 	mention = user["mention"]
