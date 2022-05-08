fa401d6dc1981ba14c8a5eabb7e921cb725518f9
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-08 16:38:23 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -12,6 +12,6 @@ Hey {}!! 💫{}✨ Is Alive .\n\n Used to generate string session.
 ALPHA_PIC = "https://te.legra.ph/file/9a207e6e453a93ab2b165.jpg"
 
 # Start Message
-@Client.on_message(filters.private & filters.incoming & filters.command("start"))
-async def yashualpha():
-    await client.send_file(ALPHA_PIC, caption=Data.START.format(msg.from_user.mention, mention), reply_markup=InlineKeyboardMarkup(Data.buttons))
+@yashu.on_message(filters.private & filters.incoming & filters.command("start"))
+async def yashualpha(event):
+    await event.send_file(ALPHA_PIC, caption=Data.START.format(msg.from_user.mention, mention), reply_markup=InlineKeyboardMarkup(Data.buttons))
