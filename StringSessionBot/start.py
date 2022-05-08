573c0312b29103671e462c5e18df991d6d28b6a1
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-08 14:51:14 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -13,8 +13,4 @@ ALPHA_PIC = "https://te.legra.ph/file/9a207e6e453a93ab2b165.jpg"
 
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
-    await client.send_file(
-                ALPHA_PIC,
-                caption=Data.START.format(msg.from_user.mention, mention),
-                reply_markup=InlineKeyboardMarkup(Data.buttons)
-         )
+    await client.send_file(ALPHA_PIC, caption=Data.START.format(msg.from_user.mention, mention), reply_markup=InlineKeyboardMarkup(Data.buttons))
