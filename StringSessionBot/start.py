73a7de35f79089d59a9bdbd3c4e1e64185a318c2
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-10 00:30:03 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -14,4 +14,4 @@ ALPHA_PIC = "https://te.legra.ph/file/9a207e6e453a93ab2b165.jpg"
 # Start Message
 @yashu.on_message(filters.private & filters.incoming & filters.command("start"))
 async def yashualpha(event):
-    await yashu.send_file(event.chat_id, ALPHA_PIC, caption=Data.START.format(msg.from_user.mention, mention), reply_markup=InlineKeyboardMarkup(Data.buttons))
+    await yashu.send_photo(event.chat_id, photo=ALPHA_PIC, caption=Data.START.format(msg.from_user.mention, mention), reply_markup=InlineKeyboardMarkup(Data.buttons))
