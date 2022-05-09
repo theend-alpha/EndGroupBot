fabd2f9bc5f4b1357d8ca9030bcd7f25bd553e17
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-10 00:47:05 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -1,17 +1,15 @@
 from Data import Data
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from YashviK import yashu
+from pyrogram.types import InlineKeyboardMarkup
 
-STGRP = """
-
-Hey {}!! 💫{}✨ Is Alive .\n\n Used to generate string session.
-
-"""
-
-ALPHA_PIC = "https://te.legra.ph/file/9a207e6e453a93ab2b165.jpg"
 
 # Start Message
-@yashu.on_message(filters.private & filters.incoming & filters.command("start"))
-async def yashualpha(event, message):
-    await yashu.send_photo(event.chat_id, photo=ALPHA_PIC, caption=Data.START.format(msg.from_user.mention, mention), reply_markup=InlineKeyboardMarkup(Data.buttons))
+@Client.on_message(filters.private & filters.incoming & filters.command("start"))
+async def start(bot, msg):
+	user = await bot.get_me()
+	mention = user["mention"]
+	await bot.send_message(
+		msg.chat.id,
+		Data.START.format(msg.from_user.mention, mention),
+		reply_markup=InlineKeyboardMarkup(Data.buttons)
+	)
