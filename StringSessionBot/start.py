c99f147f09e1bcf247dc07c7e4f3695965e549ee
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-13 11:03:06 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -7,9 +7,9 @@ photo = "https://te.legra.ph/file/9a207e6e453a93ab2b165.jpg"
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, message: Message):
-        user = await bot.get_me()
- 	mention = user["mention"]
-	await message.reply_photo(
-                photo,
-		caption=Data.START.format(message.from_user.mention, mention),
-		reply_markup=InlineKeyboardMarkup(Data.buttons))
+   user = await bot.get_me()
+   mention = user["mention"]
+   await message.reply_photo(
+      photo,
+      caption=Data.START.format(message.from_user.mention, mention),
+      reply_markup=InlineKeyboardMarkup(Data.buttons))
