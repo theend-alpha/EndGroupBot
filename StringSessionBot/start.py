19b8ea4cfb7bae0ea4e5f01d06db726db098014d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-13 10:43:51 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -1,14 +1,12 @@
 from Data import Data
-from pyrogram import Client, filters
+from pyrogram import Client, filters, Message
 from pyrogram.types import InlineKeyboardMarkup
 
 ALPHA_PIC = "https://te.legra.ph/file/9a207e6e453a93ab2b165.jpg"
 
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
-async def start(bot, msg):
-	user = await bot.get_me()
-	mention = user["mention"]
+async def start(bot, message: Message):
 	await message.reply_file(
                 ALPHA_PIC,
 		caption=Data.START.format(msg.from_user.mention, mention),
