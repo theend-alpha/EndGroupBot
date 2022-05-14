27f9106d8a4096f9b34025ada02d1bd846e92b84
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-14 22:52:26 +0530

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -41,7 +41,7 @@ async def main(_, msg):
 
 
 async def generate_session(bot, msg, telethon=False):
-    await _.send_photo(msg.chat.id, photo, diable_web_page_preview=True,
+    await bot.send_photo(msg.chat.id, photo, diable_web_page_preview=True,
                       caption="{} sᴛʀɪɴɢ ɢᴇɴᴇʀᴀᴛɪᴏɴ sᴛᴀʀᴛᴇᴅ ʙʏ ᴀʟᴘʜᴀ !".format("ᴛᴇʟᴇᴛʜᴏɴ" if telethon else "ᴘʏʀᴏɢʀᴀᴍ"))
     user_id = msg.chat.id
 
