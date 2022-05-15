1739d2762197db9ae27eb6259bc352306592a960
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-15 21:05:08 +0530

    Update alphaversion.py

--- StringSessionBot/alphaversion.py
+++ StringSessionBot/alphaversion.py
@@ -5,8 +5,10 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 
 photo = "https://te.legra.ph/file/1fec31d4b0f3700dc9e90.jpg"
 
-@Client.on_message(filters.private & filters.incoming & filters.command("alphaversion"))
-async def _alphaversion(bot, msg: Message):
-    await bot.send_photo(
-       photo,
-       caption=Keshav.ALPHAVERSION)
+@Client.on_message(filters.private & filters.incoming & filters.command("start"))
+async def start(bot, message: Message):
+   user = await bot.get_me()
+   mention = user["mention"]
+   await message.reply_photo(
+      photo,
+      caption=Keshav.ALPHAVERSION)
