48de18fae3af68308cac6077f4905c952704567e
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-15 18:11:13 +0530

    Update alphaversion.py

--- StringSessionBot/alphaversion.py
+++ StringSessionBot/alphaversion.py
@@ -1,12 +1,12 @@
 from Yashvi import Keshav
 from Data import Data
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, Message
 
 photo = "https://te.legra.ph/file/1fec31d4b0f3700dc9e90.jpg"
 
 @Client.on_message(filters.private & filters.incoming & filters.command("alphaversion"))
-async def _alphaversion(bot, msg):
+async def _alphaversion(bot, msg: Message):
     await msg.reply_photo(photo,
         Keshav.ALPHAVERSION,
         disable_web_page_preview=True,
