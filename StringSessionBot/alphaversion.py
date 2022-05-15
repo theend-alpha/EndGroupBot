5ed3c331e9e9db74d949d897b35bf94a0eedd574
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-15 20:35:36 +0530

    Update alphaversion.py

--- StringSessionBot/alphaversion.py
+++ StringSessionBot/alphaversion.py
@@ -7,6 +7,7 @@ photo = "https://te.legra.ph/file/1fec31d4b0f3700dc9e90.jpg"
 
 @Client.on_message(filters.private & filters.incoming & filters.command("alphaversion"))
 async def _alphaversion(bot, msg: Message):
-    await bot.send_photo(photo,
-        Keshav.ALPHAVERSION,
+    await bot.send_photo(
+       photo,
+       caption=Keshav.ALPHAVERSION,
     )
