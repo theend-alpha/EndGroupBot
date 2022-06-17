321d1c8d154ed8aa9661fd7cee4b03cffbb99c45
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-18 05:22:25 +0530

    Update Yashvi.py

--- Yashvi.py
+++ Yashvi.py
@@ -3,53 +3,20 @@ from pyrogram.types import InlineKeyboardButton
 
 class Keshav:
    
-    #Alpha Buttons
-    alpha_buttons = [
-         [InlineKeyboardButton(" Commands ", callback_data="cmda")],
-         [InlineKeyboardButton(" Alphaversion ", callback_data="alphaversion")]
-    ]
+    START_TXT = """ Hii !! There {}, I am {} ! \n\n {} will sends a random photo periodically containing some text ! \n\n the user who will writes the word first, will obtain coins\n\n Add me to your group and have fun ! """
 
+    start_markup = [
+        [
+        InlineKeyboardButton(" ➕ Add in your group ➕ ", url = f"https://t.me/{bn}?startgroup=true")
+        ],
+        [
+        InlineKeyboardButton(" 📖 Help ", callback_data = "help"),
+        InlineKeyboardButton(" 🛠 Tutorial ", callback_data = "tutorial")
+        ],
+        [
+        InlineKeyboardButton(" 📝 Info ", callback_data = "info"),
+        InlineKeyboardButton(" ✨ Network ", url="t.me/THE_END_NETWORK")
+        ]
+        ]
 
-    # Commands
-    CMDA = """
-** ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴀʟᴘʜᴀ ʙᴏᴛ**
-
-/start - ᴛᴏ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ✨💫\n\n/help - ᴛᴏ ᴠɪᴇᴡ ᴛʜᴇ ᴛᴜᴛᴏʀɪᴀʟ.\n\n/about - ᴅᴇᴛᴀɪʟs ᴛᴏ ᴄᴏɴᴛᴀᴄᴛ ᴛʜᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ !\n\n/alphaversion - ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴄᴜʀʀᴇɴᴛ ᴠᴇʀsɪᴏɴ ᴍᴏʀᴇ !
-
-"""
-
-    # version
-    ALPHAVERSION = """
-** ᴀʟᴘʜᴀ ᴠᴇʀsɪᴏɴ**
-
-•••> ᴠᴇʀsɪᴏɴ ɴᴀᴍᴇ - ᴇɴᴅ ᴠ𝟺 ᴜʟᴛɪᴍᴀᴛᴇ\n\n•••> ᴠᴇʀsɪᴏɴ sᴛᴀʀᴛᴇᴅ - 𝟷𝟻/𝟶𝟻/𝟸𝟶𝟸𝟸\n\n•••> ᴜᴘᴅᴀᴛᴇᴅ ʙʏ - ᴀʟᴘʜᴀ\n\n**ᴜᴘᴅᴀᴛᴇᴅ ғᴇᴀᴛᴜʀᴇs**\n\n•••> ᴀᴅᴅᴇᴅ ᴄᴏᴏʟ ᴘɪᴄ ᴡʜᴇɴ ᴛʜᴇ ᴜsᴇʀ sᴛᴀʀᴛs ᴛʜᴇ ʙᴏᴛ, ᴡɪʟʟ ᴅɪsᴘʟᴀʏᴇᴅ\n\n•••> ᴀᴅᴅᴇᴅ ᴀᴛᴛʀᴀᴄᴛɪᴠᴇ ᴄʜᴀʀᴀᴄᴛᴇʀs ᴏɴ ʙᴜᴛᴛᴏɴs\n\n•••> ᴀᴅᴅᴇᴅ ᴀᴘɪ ᴍᴇɴᴜ\n\n•••> ᴏɴʟɪɴᴇ 𝟸𝟺/𝟽\n\n•••> ʙᴜɢ ғɪxᴇs 🛠\n\n•••> ᴀɴʏ ᴍᴏʀᴇ ɪᴅᴇᴀs ғᴏʀᴡᴀʀᴅ ᴛʜᴇᴍ ᴛᴏ @NotReallyAlpha
-
-"""
-    # command buttons
-    command_buttons = [
-           [InlineKeyboardButton("Click here to Generate", callback_data="intro")],
-           [InlineKeyboardButton(text="✨Back🕊", callback_data="home")],
-           [InlineKeyboardButton(text="✨Alpha Version💫", callback_data="alphaversion")]
-    ]
-
-    # version buttons
-    version_buttons = [
-           [InlineKeyboardButton("Click here to Generate", callback_data="intro")],
-           [InlineKeyboardButton(text="✨Back🕊", callback_data="home")],
-           [InlineKeyboardButton(text="✨Commands💫", callback_data="cmda")]
-    ]
-
-    # intro buttons
-    intro_buttons = [
-         [InlineKeyboardButton("✨ Generate with Alpha db ❄️", callback_data="generate")],
-         [InlineKeyboardButton("✨ Generate with your API ❄️", callback_data="generatee")]
-    ]
         
-    INTRO = """
-** ᴀᴘɪ ᴍᴇɴᴜ **
-
-•••> ʜᴇʟʟᴏ✨💫! ᴛʜɪs ɪs ᴛʜᴇ ᴀᴅᴠᴀɴᴄᴇᴅ sᴛʀɪɴɢ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ !\n\n•••> ʜᴇʀᴇ ʏᴏᴜ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ʏᴏᴜʀ sᴛʀɪɴɢ ᴡɪᴛʜ ᴀɴᴅ ᴡɪᴛʜᴏᴜᴛ ᴀᴘɪ ❄️🕊.\n\n•••> [ᴅᴇᴠ](t.me/iTz_DEv_xD) - [ᴀʟᴘʜᴀ](t.me/NotReallyAlpha)\n
-
-"""
-
-         
