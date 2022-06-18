94fc606cd1d0ecfafd6d76cb638f5f1bd2473c3d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-18 10:09:14 +0530

    Create ownerstuff.py

---
+++ EndFW/ownerstuff.py
@@ -0,0 +1,17 @@
+from pyrogram import Client as End, filters
+from pyrogram.types import Message as Dev
+from EndFW.AlphaDatabase.users import *
+
+@End.on_message(filters.command("users") & filters.user(ALPHA_ID) & ~filters.edited & ~filters.forwarded)
+async def users(Alpha, Doli: Dev):
+    users = get_users()
+    pusers = get_pusers()
+    USERS = []
+    PUSERS = []
+    for user in users:
+        USERS.append(user.id)
+    for puser in pusers:
+        PUSERS.append(puser.id)
+    no_users = len(USERS)
+    no_pusers = len(PUSERS)
+    await Doli.reply(f"*Total Users* \n\n • Count :- {no_users} \n\n*Private Users* \n\n • Count :- {no_pusers}")
