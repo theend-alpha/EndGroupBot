4040148adc5325dac1c83a59d1326f46459ccfd5
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-18 06:18:08 +0530

    Update start.py

--- EndFW/start.py
+++ EndFW/start.py
@@ -4,10 +4,19 @@ from Yashvi import Keshav
 from Config import BOT_USERNAME as bn
 
 @End.on_message(filters.command(["start", f"start@{bn}"]) & ~filters.edited & ~filters.forwarded & ~filters.via_bot & filters.private)
-async def (Alpha, Doli: Dev):
-    uid = Doli.from_user.id
-    nayan = await Alpha.get_users(uid)
+async def p_start(Alpha, Doli: Dev):
+    id = Doli.from_user.id
+    nayan = await Alpha.get_users(id)
     tara = nayan.mention
     me = await Alpha.get_me()
     mention = me["mention"]
+    add_user(id)
+    private_user(id)
     await Doli.reply(Keshav.START_TXT.format(tara, mention), reply_markup=InlineKeyboardMarkup(Keshav.start_markup))
+
+@End.on_message(filters.command(["start", f"start@{bn}"]) & filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded)
+async def g_start(Alpha, Doli: Dev):
+    id = Doli.from_user.id
+    tara = (await Alpha.get_users(id)).mention
+    add_user(id)
+    await Alpha.send_message(Doli.chat.id, f"Hey! {tara}, start me in pm for help", reply_markup=InlineKeyboardMarkup(" Start ➡️ ", url=f"t.me/{bn}"))
