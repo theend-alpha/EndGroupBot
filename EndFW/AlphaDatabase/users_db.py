c813e94f5514a636ca32d9bd7b860000bcb55a16
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-18 05:56:03 +0530

    Update users_db.py

--- EndFW/AlphaDatabase/users_db.py
+++ EndFW/AlphaDatabase/users_db.py
@@ -1,1 +1,12 @@
 from AlphaDatabase import SESSION as YashuAlpha, BASE
+from sqlalchemy import Column, Integer
+
+class Users(BASE):
+    __tablename__ = "users"
+
+    uid = Column(Integer, primary_key=True)
+
+   def __init__(self, uid)
+       self.uid = uid
+
+Users.__table__.create(checkfirst=True)
