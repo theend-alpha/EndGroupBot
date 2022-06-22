98460fa925f3a4d87e5e5f5efc686f994df46e9b
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-22 23:14:11 +0530

    Update start.py

--- EndGB/EndNetwork/start.py
+++ EndGB/EndNetwork/start.py
@@ -1,2 +1,6 @@
 from pyrogram import Client as End, filters
 from pyrogram.types import Message as Dev
+from EndGB.EndHelpers import *
+
+@End.on_message(filters.command(["start", "start@EndGroupBot"]) & filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarder)
+async def start(maharaj, Dev):
