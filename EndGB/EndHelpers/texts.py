7d268ed81b1c7de74df7bb663f572ded5119389f
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-22 23:06:25 +0530

    Create texts.py

---
+++ EndGB/EndHelpers/texts.py
@@ -0,0 +1,2 @@
+START_TXT = """Hi, My name is End, I'll help you to manage your groups. Hit /help for more about how to use this bot. \n\n join my channel @THE_END_NETWORK to explore updates and more bots \n\n personal contact refers to owner of @THE_END_NETWORK\n\n @Deveshi and @timeisnotwaiting """
+
