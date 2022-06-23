045934c4227805253d61ddbc9544a9d75d660023
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-23 14:20:13 +0530

    Update start.py

--- EndGB/EndNetwork/start.py
+++ EndGB/EndNetwork/start.py
@@ -8,6 +8,10 @@ DEV_P = "https://te.legra.ph/file/2a431e33cc12f456ec4ce.jpg"
 async def start(maharaj, Dev):
     await maharaj.send_photo(Dev.chat.id, DEV_P, caption=START_TXT, reply_markup=InlineKeyboardMarkup(START_MARKUP))
 
-@End.on_message(filters.command(["start", "start@EndGroupBot"]) & filters.group & ~filters.via_bot & ~filters.forwarded)
+@End.on_message(filters.command(["start", "start@EndCrystalBot"]) & filters.group & ~filters.via_bot & ~filters.forwarded)
 async def gstart(maharaj, Dev):
-    await Dev.reply("Ping me for help", reply_markup=InlineKeyboardMarkup(InlineKeyboardButton("Start in Pm", url="t.me/EndGroupBot")))
+    await Dev.reply("Ping me for help", reply_markup=InlineKeyboardMarkup(InlineKeyboardButton("Start in Pm", url="t.me/EndCrystalBot")))
+
+@End.on_message(filters.command(["help", "help@EndCrystalBot"]) & filters.private & ~filters.edited)
+async def help(_, m: Dev):
+    await m.reply(HELP_TXT)
