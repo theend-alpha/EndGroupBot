9fab53e767feb181f38330b90c2ab5e36701b18b
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-23 07:31:31 +0530

    Update emojify.py

--- EndGB/EndNetwork/emojify.py
+++ EndGB/EndNetwork/emojify.py
@@ -6,7 +6,7 @@ from EndGB.EndAddons.emotes import *
 async def doli(ailika, jhulika: Dev):
         txt = jhulika.text
         if jhulika.command == 1:
-            await jhulika.reply("Try: < /doli crystal >")
+            return await jhulika.reply("Try: < /doli crystal >")
         txt = txt.split(None, 1)[1]
         final = ""
         for a in txt:
