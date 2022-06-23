cffdb05a55cc73644c96eacfc9bf8b08dbecc6f7
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-23 10:41:38 +0530

    Update ban.py

--- EndGB/EndNetwork/ban.py
+++ EndGB/EndNetwork/ban.py
@@ -2,3 +2,6 @@ from pyrogram import Client as End, filters
 from pyrogram.types import Message as Dev
 
 BAN_TEXT = "{} is banned by {} !"
+
+@End.on_message(filters.command("ban") & filters.group & ~filters.forwarded & ~filters.edited & ~filters.via_bot)
+async def ban(alpha, keshav: Dev):
