65d41cd9f467d3f94b2ae5f5a95e6d182876263e
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-25 12:25:39 +0530

    Create mentionall.py

---
+++ EndGB/EndNetwork/mentionall.py
@@ -0,0 +1,18 @@
+from pyrogram import Client as Crystal, filters
+from pyrogram.types import Message as Aila
+
+@Crystal.on_message(filters.command(["all", "tagall"]) & filters.group & ~filters.edited & ~filters.forwarded & ~filters.via_bot)
+async def all(Kiddo, taenii: Aila):
+    if len(taenii.command) == 2:
+        arre = taenii.text.split(None, 1)[1]
+        hehe = arre
+    else:
+        hehe = "Hello everyone"
+    users = await Kiddo.iter_chat_members(taenii.chat.id)
+    mentions = """"""
+    for user in users:
+        mention = user.user.mention
+        mentions += f"\n{mention}"
+    lel = f"{hehe}\n{mentions}"
+    await Kiddo.send_message(taenii.chat.id, lel) 
+    
