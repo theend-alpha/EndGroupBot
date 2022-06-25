a1b94e8e53db60d4ef7707ace88da3c6b76751ff
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-25 23:14:25 +0530

    Update sudos_cdb.py

--- EndGB/EndDB/sudos_cdb.py
+++ EndGB/EndDB/sudos_cdb.py
@@ -1,54 +1,57 @@
 from . import SESSION, BASE
 from sqlalchemy import Column, Integer
 
-class Sudo(BASE):
-    __tablename__ = "sudo"
+class SUDO(BASE):
+    __tablename__ = "sudousers"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
 
     def __init__(self, id):
         self.id = id
 
-Sudo.__table__.create(checkfirst=True)
+SUDO.__table__.create(checkfirst=True)
+
+SUDO_IL = threading.RLock()
 
 def add_sudo(id):
-    is_sudo = SESSION.query(Sudo).get(id)
-    if not is_sudo:
-        adder = Sudo(id)
-        SESSION.add(adder)
-        SESSION.commit()
-    else:
-        SESSION.close()
-
+    with SUDO_IL:
+        is_sudo = SESSION.query(SUDO).get(id)
+        if not is_sudo:
+            adder = SUDO(id)
+            SESSION.add(adder)
+            SESSION.commit()
+        else:
+            SESSION.close()
+    
 def del_sudo(id):
-    is_sudo = SESSION.query(Sudo).get(id)
-    if is_sudo:
-        SESSION.delete(is_sudo)
-        SESSION.commit()
-    else:
+    with SUDO_IL:
+        is_sudo = SESSION.query(SUDO).get(id)
+        if is_sudo:
+            SESSION.delete(is_sudo)
+            SESSION.commit()
+        else:
+            SESSION.close()
+
+def list_all_sudos():
+    sudos = SESSION.query(SUDO).all()
+    try:
+        return sudos
+    finally:
         SESSION.close()
 
 def is_sudo(id):
-    sudo = SESSION.query(Sudo).get(id)
+    sudo = SESSION.query(SUDO).get(id)
     if sudo:
         return True
     else:
         return False
 
 def clr_all_sudos():
-    sudos = SESSION.query(Sudo).all()
-    SUDOS = []
+    OMFOO = []
+    sudos = SESSION.query(SUDO).all()
     for sudo in sudos:
-        SUDOS.append(sudo.id)
-    for SUDO in SUDOS:
-        lel = SESSION.query(Sudo).get(SUDO)
-        SESSION.delete(lel)
+        OMFOO.append(sudo.id)
+    for baka in OMFOO:
+        shivi = SESSION.query(SUDO).get(baka)
+        SESSION.delete(shivi)
         SESSION.commit()
-
-def list_all_sudos():
-    sudos = SESSION.query(Sudo).all()
-    try:
-        return sudos
-    finally:
-        SESSION.close()
-
