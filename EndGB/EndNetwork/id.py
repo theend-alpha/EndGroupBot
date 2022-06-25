399a698abeafa0fa8fff652031183e5b73cc2b3b
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-26 01:03:22 +0530

    Create id.py

---
+++ EndGB/EndNetwork/id.py
@@ -0,0 +1,12 @@
+from pyrogram import Client as Crystal, filters
+from pyrogram.types import Message as Aila
+from EndGB.EndDB.blocks_cdb import is_blocked
+
+@Crystal.on_message(filters.command(["id", "id@EndCrystalBot"]) & ~filters.edited & ~filters.via_bot)
+async def id(_, m: Aila):
+    if m.reply_to_message:
+        f_n = m.reply_to_message.from_user.first_name
+        id = m.reply_to_message.from_user.id
+    else:
+        await m.reply("reply to a user")
+    await m.reply(f"{f_n} has an id of <i>{id}</i>")
