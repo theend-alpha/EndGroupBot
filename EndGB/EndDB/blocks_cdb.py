f4a05444b24e16e4b1ab4f01fa7e49177d4b71d0
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-26 00:04:07 +0530

    Update blocks_cdb.py

--- EndGB/EndDB/blocks_cdb.py
+++ EndGB/EndDB/blocks_cdb.py
@@ -1,31 +1,44 @@
 from . import SESSION, BASE
 from sqlalchemy import Column, Integer
+from sqlalchemy.sql.sqltypes import BigInteger
+import threading 
 
 class Block(BASE):
-    __tablename__ = "block"
+    __tablename__ = "blocks"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
 
     def __init__(self, id):
         self.id = id
 
 Block.__table__.create(checkfirst=True)
 
-def block_user(id):
-    is_sudo = SESSION.query(Block).get(id)
-    if not is_sudo:
-        adder = Block(id)
-        SESSION.add(adder)
-        SESSION.commit()
-    else:
-        SESSION.close()
+Block_IL = threading.RLock()
 
+def block_user(id):
+    with Block_IL:
+        is_sudo = SESSION.query(Block).get(id)
+        if not is_sudo:
+            adder = SUDO(id)
+            SESSION.add(adder)
+            SESSION.commit()
+        else:
+            SESSION.close()
+    
 def unblock_user(id):
-    is_sudo = SESSION.query(Block).get(id)
-    if is_sudo:
-        SESSION.delete(is_sudo)
-        SESSION.commit()
-    else:
+    with Block_IL:
+        is_sudo = SESSION.query(Block).get(id)
+        if is_sudo:
+            SESSION.delete(is_sudo)
+            SESSION.commit()
+        else:
+            SESSION.close()
+
+def list_all_blocked():
+    sudos = SESSION.query(Block).all()
+    try:
+        return sudos
+    finally:
         SESSION.close()
 
 def is_blocked(id):
@@ -34,20 +47,3 @@ def is_blocked(id):
         return True
     else:
         return False
-
-def clr_all_blocked():
-    sudos = SESSION.query(Block).all()
-    SUDOS = []
-    for sudo in sudos:
-        SUDOS.append(sudo.id)
-    for SUDO in SUDOS:
-        lel = SESSION.query(Block).get(SUDO)
-        SESSION.delete(lel)
-        SESSION.commit()
-
-def list_all_blocked():
-    sudos = SESSION.query(Block).all()
-    try:
-        return sudos
-    finally:
-        SESSION.close()
