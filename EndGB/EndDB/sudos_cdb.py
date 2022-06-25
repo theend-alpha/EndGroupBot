2a03486162a82f80fb76b1b557d30fbc4b99b035
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-25 18:55:52 +0530

    Update and rename sudos_edb.py to sudos_cdb.py

---
+++ EndGB/EndDB/sudos_cdb.py
@@ -0,0 +1,36 @@
+from . import SESSION, BASE
+from sqlalchemy import Column, Integer
+
+class Sudo(BASE):
+    __tablename__ = "sudo"
+
+    user_id = Column(Integer, primary_key=True)
+
+    def __init__(self, user_id):
+        self.user_id = user.id
+
+Sudo.__table__.create(checkfirst=True)
+
+def add_sudo(i):
+    is_sudo = SESSION.query(Sudo).get(i)
+    if not is_sudo:
+        adder = Sudo(i)
+        SESSION.add(adder)
+        SESSION.commit()
+    else:
+        SESSION.close()
+
+def del_sudo(i):
+    is_sudo = SESSION.query(Sudo).get(i)
+    if is_sudo:
+        SESSION.delete(is_sudo)
+        SESSION.commit()
+    else:
+        SESSION.close()
+
+def is_sudo(i):
+    sudo = SESSION.query(Sudo).get(i)
+    if sudo:
+        return True
+    else:
+        return False
--- EndGB/EndDB/sudos_edb.py
+++
@@ -1,7 +0,0 @@
-from . import SESSION, BASE
-from sqlalchemy import Column, Integer
-
-class Sudo(BASE):
-    __tablename__ = "sudo"
-
-    user_id = Column(Integer, primary_key=True)
