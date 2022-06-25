171386067703b7a64a71139b169f5e99dafbe91d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-25 12:36:05 +0530

    Update markups.py

--- EndGB/EndHelpers/markups.py
+++ EndGB/EndHelpers/markups.py
@@ -5,3 +5,9 @@ START_MARKUP = [
     InlineKeyboardButton("➕ Add to your chat ➕", url=f"t.me/EndCrystalBot?startgroup=true")
     ]
     ]
+
+HELP_MARKUP = [
+    [
+    InlineKeyboardButton("✨ start in pm 💫", url="t.me/EndCrystalBot")
+    ]
+    ]
