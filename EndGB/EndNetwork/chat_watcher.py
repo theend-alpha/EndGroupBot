32ee149072120e6036cd9662baffb490889c47d1
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-27 21:21:46 +0530

    Update chat_watcher.py

--- EndGB/EndNetwork/chat_watcher.py
+++ EndGB/EndNetwork/chat_watcher.py
@@ -2,7 +2,7 @@ from pyrogram import Client as Crystal, filters
 from pyrogram.types import Message as Aila
 from EndGB.EndDB.servers_cdb import *
 
-@Crsytal.on_message(group=1)
+@Crystal.on_message(group=1)
 async def cwfunc(_, m):
     chat_id = m.chat.id
     if is_served_chat(chat_id):
