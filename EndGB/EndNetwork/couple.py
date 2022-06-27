2cd8a009478c295cc8a6ce9b05c740b37ace0c0b
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-28 00:45:21 +0530

    Update couple.py

--- EndGB/EndNetwork/couple.py
+++ EndGB/EndNetwork/couple.py
@@ -5,9 +5,9 @@ import datetime as dt
 
 year_month_date = dt.date.today()
 y_m_d = str(year_month_date)
-year = y_m_d[0:4]
-month = y_m_d[5:7]
-date = y_m_d[8:]
+year = int(y_m_d[0:4])
+month = int(y_m_d[5:7])
+date = int(y_m_d[8:])
 
 @Crystal.on_message(filters.command(["couple", "couples", "shipping", "shipping@EndCrystalBot"]) & ~filters.edited & ~filters.via_bot)
 async def couple(_, m: Aila):
@@ -18,3 +18,7 @@ async def couple(_, m: Aila):
             for couple in couples:
                 COUPLE.append(couple.c1_id)
                 COUPLE.append(couple.c2_id)
+            c1_id = COUPLE[0]
+            c2_id = COUPLE[1]
+            c1_m = (await _.get_users(c1_id)).mention
+            c2_m = (await _.get_users(c2_id)).mention
