0a5e5f93c9089371eb8d097170e6ec0eec319b51
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-28 00:36:11 +0530

    Update couple.py

--- EndGB/EndNetwork/couple.py
+++ EndGB/EndNetwork/couple.py
@@ -1,2 +1,13 @@
 from pyrogram import Client as Crystal, filters
 from pyrogram.types import Message as Aila
+from EndGB.EndDB.couples_cdb import *
+import datetime as dt 
+
+year_month_date = dt.date.today()
+y_m_d = str(year_month_date)
+year = y_m_d[0:4]
+month = y_m_d[5:7]
+date = y_m_d[8:]
+
+@Crystal.on_message(filters.command(["couple", "couples", "shipping", "shipping@EndCrystalBot"]) & ~filters.edited & ~filters.via_bot)
+async def couple(_, m: Aila):
