942d641a966914b08590cc48b9c325b7d5b261f6
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-27 09:15:00 +0530

    Update ban.py

--- EndGB/EndNetwork/ban.py
+++ EndGB/EndNetwork/ban.py
@@ -77,6 +77,9 @@ async def unban(_, m: Aila):
         pass
     if m.from_user:
         if member.can_restrict_members or is_sudo(i_id) or i_id in ALPHA_ID:
+            lel = await _.get_chat_member(m.chat.id, f_id)
+            if lel.status != "banned":
+                return await m.reply("user isn't banned")
             f_m = (await _.get_users(f_id)).mention
             f_un = (await _.get_users(f_id)).username
             await _.unban_chat_member(m.chat.id, f_id)
