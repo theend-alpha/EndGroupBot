dbfa3f80fdbd73b98f50d55b7810ccb3586a2437
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-28 16:38:04 +0530

    Update id.py

--- EndGB/EndNetwork/id.py
+++ EndGB/EndNetwork/id.py
@@ -7,6 +7,18 @@ async def id(_, m: Aila):
     if m.reply_to_message:
         f_n = m.reply_to_message.from_user.first_name
         id = m.reply_to_message.from_user.id
-        await m.reply(f"{f_n} has an ID of <code>{id}</code>.")
+        return await m.reply(f"{f_n} has an ID of <code>{id}</code>.")
+    elif len(m.command) == 2:
+        hehe = m.text.split(None, 1)[1]
+        if hehe[0] == "@":
+            try:
+                id = (await _.get_users(hehe)).id
+            except:
+                return await m.reply("username given is invalid 🤧")
+        else:
+            return await m.reply("Try: <code>/id [username]</code>")
     else:
-        await m.reply(f"user {m.from_user.first_name} has an ID of <code>{m.from_user.id}</code>, chat has an ID <code>{m.chat.id}</code>.")
+        if m.chat.type == "private":
+            return await m.reply(f"your ID is <code>{m.from_user.id}</code>")
+        else:
+            return await m.reply(f"user {m.from_user.first_name} has an ID of <code>{m.from_user.id}</code>, chat has an ID <code>{m.chat.id}</code>.")
