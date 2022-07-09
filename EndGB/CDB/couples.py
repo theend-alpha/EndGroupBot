d8ecf13b4f4243f75a814c1d7cc0fb4f9d5ba38b
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-10 00:03:41 +0530

    Update couples.py

--- EndGB/CDB/couples.py
+++ EndGB/CDB/couples.py
@@ -1,1 +1,10 @@
 from EndGB.CDB import db as rup
+
+couplesdb = rup.couples
+
+async def add_couple(a, b, d, m, y):
+    getter = await couplesdb.find_one({"a": a}, {"b": b}, {"d": d}, {"m": m}, {"y": y})
+    if getter:
+        return
+    await couplesdb.insert_one({"a": a}, {"b": b}, {"d": d}, {"m": m}, {"y": y})
+
