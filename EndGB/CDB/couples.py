c3a1aeffdddf87c4450c6e46a376621f1c9835d5
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-11 04:03:15 +0530

    Update couples.py

--- EndGB/CDB/couples.py
+++ EndGB/CDB/couples.py
@@ -2,20 +2,20 @@ from EndGB.CDB import db as rup
 
 couplesdb = rup.couples
 
-async def add_couple(a, b, d, m, y):
+async def add_couple(a: int, b: int, d: int, m: int, y: int):
     getter = await couplesdb.find_one({"a": a}, {"b": b}, {"d": d}, {"m": m}, {"y": y})
     if getter:
         return
     await couplesdb.insert_one({"a": a}, {"b": b}, {"d": d}, {"m": m}, {"y": y})
 
-async def check_couple(d, m, y):
+async def check_couple(d: int, m: int, y: int):
     getter = await couplesdb.find_one({"d": d}, {"m": m}, {"y": y}, {"a": {"$gt": 0}}, {"b": {"$gt": 0}})
     if getter:
         return True
     else:
         return False
 
-async def get_couple(d, m, y):
+async def get_couple(d: int, m: int, y: int):
     getter = await couplesdb.find_one({"d": d}, {"m": m}, {"y": y}, {"a": {"$gt": 0}}, {"b": {"$gt": 0}})
     try:
         return getter
