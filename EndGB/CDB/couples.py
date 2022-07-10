2c9e110d3ab84a9aa741de477878345cd3acab3f
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-10 11:45:04 +0530

    Update couples.py

--- EndGB/CDB/couples.py
+++ EndGB/CDB/couples.py
@@ -9,14 +9,14 @@ async def add_couple(a, b, d, m, y):
     await couplesdb.insert_one({"a": a}, {"b": b}, {"d": d}, {"m": m}, {"y": y})
 
 async def check_couple(d, m, y):
-    getter = await couplesdb.find_one({"d": d}, {"m": m}, {"y": y}, {"a": {$gt: 0}}, {"b": {$gt: 0}})
+    getter = await couplesdb.find_one({"d": d}, {"m": m}, {"y": y}, {"a": {"$gt": 0}}, {"b": {"$gt": 0}})
     if getter:
         return True
     else:
         return False
 
 async def get_couple(d, m, y):
-    getter = await couplesdb.find_one({"d": d}, {"m": m}, {"y": y}, {"a": {$gt: 0}}, {"b": {$gt: 0}})
+    getter = await couplesdb.find_one({"d": d}, {"m": m}, {"y": y}, {"a": {"$gt": 0}}, {"b": {"$gt": 0}})
     try:
         return getter
     except:
