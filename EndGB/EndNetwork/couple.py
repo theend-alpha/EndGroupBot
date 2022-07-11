9370ee4dc58964bb78a0b1333fb2a4a393c34d2a
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-11 09:39:28 +0530

    Update couple.py

--- EndGB/EndNetwork/couple.py
+++ EndGB/EndNetwork/couple.py
@@ -27,28 +27,28 @@ async def cpl(_, m):
 New couple of the day may be chosen at 5 : 30 am next day!"""
             ok = await _.send_message(m.chat.id, csm)
         
-        if ok:
-            return
-        else:
-            KIDS = []
-            async for user in _.iter_chat_members(m.chat.id)
-                if user.user.is_bot:
-                    return
-                KIDS.append(user.user.id)
-            if len(KIDS) < 2:
-                await m.reply("Not enough users to ship ")
-            c1 = random.choice(KIDS)
+    if ok:
+        return
+    else:
+        KIDS = []
+        async for user in _.iter_chat_members(m.chat.id)
+            if user.user.is_bot:
+                return
+            KIDS.append(user.user.id)
+        if len(KIDS) < 2:
+            await m.reply("Not enough users to ship ")
+        c1 = random.choice(KIDS)
+        c2 = random.choice(KIDS)
+        while c1 == c2:
             c2 = random.choice(KIDS)
-            while c1 == c2:
-                c2 = random.choice(KIDS)
-            c1_m = (await _.get_users(c1)).mention
-            c2_m = (await _.get_users(c2)).mention
-            csm = f"""Couple of the day:\n\n
+        c1_m = (await _.get_users(c1)).mention
+        c2_m = (await _.get_users(c2)).mention
+        csm = f"""Couple of the day:\n\n
 {c1_m} + {c2_m} = ❤️\n\n
 New couple of the day may be chosen at 5 : 30 am next day!"""
-            lel = f"{c1} {c2} {date} {month} {year}"
-            await add_couple(lel)
-            await _.send_message(m.chat.id, csm)
+        lel = f"{c1} {c2} {date} {month} {year}"
+        await add_couple(lel)
+        await _.send_message(m.chat.id, csm)
 
 
      
